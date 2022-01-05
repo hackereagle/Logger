b7e25343dad51d56ad782c5110c0c3fd665aa8cf
世智 林 <read0651@hotmail.com>
2022-01-05 09:44:26 +0800

    2022.01.05 sc.lin Chg: Added some function for C# PInvoke.

--- src/Logger/WrapperForDotNet.hpp
+++ src/Logger/WrapperForDotNet.hpp
@@ -2,3 +2,15 @@
 
 #include "LoggerCommon.h"
 #include "Logger.h"
+
+#define LOGLEVEL_VALUE_TABLE(a, b, c) a b
+#define LOGLEVEL_VALUE_TABLE
+#undef LOGLEVEL_TABLE
+
+#if _WIN32
+extern "C" {
+DLLEXPORT_LOGGER_API int InitializeLogger();
+}
+#else
+int InitializeLogger(int a = LOGALL);
+#endif // _WIN32
